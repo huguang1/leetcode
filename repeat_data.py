de759807961d881192e136539730aa63b98ccfa2
huguang <1349291824@qq.com>
2022-12-04 17:19:44 +0000

    1

---
+++ repeat_data.py
@@ -0,0 +1,22 @@
+def containsNearbyDuplicate(nums, k) -> bool:
+    dict_num = {}
+    small_g = k + 1
+    for i, v in enumerate(nums):
+        if v not in dict_num.keys():
+            dict_num[v] = [i]
+        else:
+            for a in dict_num[v]:
+                if small_g > abs(i - a):
+                    small_g = abs(i - a)
+            dict_num[v].append(i)
+
+    if small_g <= k:
+        return True
+    else:
+        return False
+
+
+nums = [1, 2, 3, 1]
+k = 3
+print(containsNearbyDuplicate(nums, k))
+
